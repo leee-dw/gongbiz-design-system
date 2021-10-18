e3bf41f136ab136533e136a814416f3c27234c0a
leee-dw <dylan.dw.lee@gmail.com>
2021-10-18 17:54:52 +0900

    version: 1.0.9

--- rollup.config.js
+++ rollup.config.js
@@ -20,30 +20,11 @@ export default {
     peerDepsExternal(),
     image(),
     resolve(),
-    commonjs({
-      include: /node_modules/,
-    }),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss({
       extract: false,
       modules: true,
       use: ['sass'],
     }),
-    {
-      name: 'Custom Rollup Plugin`',
-
-      generateBundle: (options, bundle) => {
-        Object.entries(bundle).forEach(entry => {
-          if (!entry[0].match(/.*(.scss.js)$/)) {
-            return
-          }
-
-          bundle[entry[0]].code = entry[1].code.replace(
-            '../../node_modules/style-inject/dist/style-inject.es.js',
-            'style-inject',
-          )
-        })
-      },
-    },
   ],
 }
