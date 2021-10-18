758434454b98966d0d2745ce838f29ea984ec962
leee-dw <dylan.dw.lee@gmail.com>
2021-10-18 19:00:28 +0900

    version: 1.0.15

--- build/components/Button/index.module.scss.js
+++
@@ -1,8 +0,0 @@
-import styleInject from 'style-inject';
-
-var css_248z = ".index-module_default__107Bz {\n  padding: 12px 24px;\n  background-color: #495057;\n  color: white;\n  border: #323540;\n  border-radius: 0.25rem;\n  cursor: pointer;\n  transition: all 2000 ease; }\n  .index-module_default__107Bz:hover {\n    background-color: #343a40; }\n  .index-module_default__107Bz:active {\n    background-color: #212529; }\n\n.index-module_primary__1GMxA {\n  background-color: #f03e3e;\n  border: #f03e3e;\n  color: white; }\n  .index-module_primary__1GMxA:hover {\n    background-color: #e03131; }\n  .index-module_primary__1GMxA:active {\n    background-color: #c92a2a; }\n\n.index-module_secondary__2tv-b {\n  background-color: #d6336c;\n  border: #d6336c;\n  color: white; }\n  .index-module_secondary__2tv-b:hover {\n    background-color: #c2255c; }\n  .index-module_secondary__2tv-b:active {\n    background-color: #a61e4d; }\n";
-var styles = {"default":"index-module_default__107Bz","primary":"index-module_primary__1GMxA","secondary":"index-module_secondary__2tv-b"};
-styleInject(css_248z);
-
-export { styles as default };
-//# sourceMappingURL=index.module.scss.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -4,25 +4,34 @@ import typescript from 'rollup-plugin-typescript2'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from '@rollup/plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+import external from 'rollup-plugin-peer-deps-external'
+import svgr from '@svgr/rollup'
+import url from 'rollup-plugin-url'
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'] // 어떤 확장자를 처리 할 지 정함
+process.env.BABEL_ENV = 'production'
 
 export default {
-  input: './index.ts',
+  input: './src/index.ts',
   output: [
     {
-      dir: 'build',
-      format: 'esm',
-      exports: 'named',
-      sourcemap: true,
+      file: pkg.module, // 번들링한 파일을 저장 할 경로
+      format: 'es', // ES Module 형태로 번들링함
     },
   ],
-  preserveModules: true,
+
   plugins: [
     peerDepsExternal(),
     image(),
-    resolve(),
+    resolve({ extensions }),
     commonjs({
-      include: /node_modules/,
+      include: 'node_modules/**',
     }),
+    babel({ extensions, include: ['src/**/*'], runtimeHelpers: true }), // Babel을 사용 할 수 있게 해줌
+    url(), // 미디어 파일을 dataURI 형태로 불러와서 사용 할 수 있게 해줌.
+    svgr(), // SVG를 컴포넌트로 사용 할 수 있게 해줌.
     typescript({ useTsconfigDeclarationDir: true }),
     postcss({
       extract: false,
