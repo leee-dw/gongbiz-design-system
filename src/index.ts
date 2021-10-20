b6c188387bda74046838839d92d78eea93c9400d
leee-dw <dylan.dw.lee@gmail.com>
2021-10-20 18:01:15 +0900

    v1.0.36

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './components/Button/Button'
 export { default as ButtonGroup } from './components/ButtonGroup/ButtonGroup'
+export { default as Input } from './components/Input/Input'
 export { default as Icon } from './components/Icons/Icon'
